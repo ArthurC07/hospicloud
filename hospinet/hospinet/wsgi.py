860a81b58ba3275bbea9ca1d0125b3f76af9a3be
Carlos Flores <cafg10@gmail.com>
2013-11-11 17:04:57 -0600

    Actualizar wsgi.py

--- hospinet/hospinet/wsgi.py
+++ hospinet/hospinet/wsgi.py
@@ -14,31 +14,34 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
+"""
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
+"""
 import os
-import sys
-from hospinet import settings
 
-program, subplace = os.path.split(os.path.dirname(__file__))
-#program, subplace = os.path.split(program)
-path = os.path.dirname(__file__)
-sys.path.append(program)
+# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
+# if running multiple sites in the same mod_wsgi process. To fix this, use
+# mod_wsgi daemon mode with each site in its own daemon process, or use
+# os.environ["DJANGO_SETTINGS_MODULE"] = "benchmark.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hospinet.settings")
 
-if path not in sys.path:
-    sys.path.append(path)
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
 
-import django.core.management
-django.core.management.setup_environ(settings)
-utility = django.core.management.ManagementUtility()
-command = utility.fetch_command('runserver')
+application = get_wsgi_application()
 
-command.validate()
-
-import django.conf
-import django.utils
-
-django.utils.translation.activate(django.conf.settings.LANGUAGE_CODE)
-
-import django.core.handlers.wsgi
-
-application = django.core.handlers.wsgi.WSGIHandler()
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
