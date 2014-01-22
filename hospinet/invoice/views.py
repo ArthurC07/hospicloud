7219166878b54c9d869f3724d227068dcd510813
Carlos Flores <cafg10@gmail.com>
2014-01-22 08:57:13 -0600

    Utilizar version correcta de jquery UI

--- hospinet/invoice/views.py
+++ hospinet/invoice/views.py
@@ -319,8 +319,7 @@ class IndexView(TemplateView, LoginRequiredMixin):
         context['inventarioform'] = InventarioForm(prefix='inventario')
         context['inventarioform'].set_action('invoice-inventario')
 
-        context['examenes'] = Examen.objects.filter(facturado=False).order_by(
-            id).all()
+        context['examenes'] = Examen.objects.filter(facturado=False)
         context['admisiones'] = Admision.objects.filter(facturada=False)
         context['emergencias'] = Emergencia.objects.filter(facturada=False)
 
