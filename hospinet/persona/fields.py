b15865d7becb1a4e9a7484a6a31cefa6ba6d68ee
Carlos Flores <cafg10@gmail.com>
2014-02-25 08:12:18 -0600

    Actualizar a django-countries 2.0

--- hospinet/persona/fields.py
+++ hospinet/persona/fields.py
@@ -15,50 +15,53 @@
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
 from django_countries.fields import CountryField
-from django_countries.countries import COUNTRIES
+from django_countries.data import COUNTRIES
 from django.db.models.fields import CharField
 from south.modelsinspector import add_introspection_rules
 
-class OrderedCountryField(CountryField):
 
+class OrderedCountryField(CountryField):
     """Permite mostrar algunos códigos de país antes que el resto"""
-    
+
     def __init__(self, *args, **kwargs):
-        
+
         choices = COUNTRIES
         ordered = kwargs.pop('ordered', None)
         sort = kwargs.pop('sort', None)
         if sort:
             from operator import itemgetter
+
             if sort == 'Name':
                 choices = sorted(choices, key=itemgetter(1))
             else:
-                pass #right now choices is sorted by code already
+                pass  #right now choices is sorted by code already
         if ordered:
             choices_in_ordered = {}
             ordered_choices = []
             other_choices = []
-            for k, v in choices:
-                if k in ordered:
-                    choices_in_ordered[k] = v
+            for code in choices:
+                if code in ordered:
+                    choices_in_ordered[code] = choices[code]
                 else:
-                    other_choices.append((k, v))
+                    other_choices.append((code, choices[code]))
             for o in ordered:
                 ordered_choices.append((o, choices_in_ordered[o]))
             choices = tuple(ordered_choices + other_choices)
-        
-        kwargs.setdefault('max_length', 2) 
-        kwargs.setdefault('choices', choices) 
+
+        kwargs.setdefault('max_length', 2)
+        kwargs.setdefault('choices', choices)
 
         super(CharField, self).__init__(*args, **kwargs)
 
+
 add_introspection_rules([
-    (
-        [OrderedCountryField], # Class(es) these apply to
-        [],         # Positional arguments (not used)
-        {           # Keyword argument
-            #"ordered": ["ordered", {}],
-            #"sort": ["sort", {}],
-        },
-    ),
-], ["^persona\.fields\.OrderedCountryField"])
+                            (
+                                [OrderedCountryField],
+                                # Class(es) these apply to
+                                [],  # Positional arguments (not used)
+                                {  # Keyword argument
+                                   #"ordered": ["ordered", {}],
+                                   #"sort": ["sort", {}],
+                                },
+                            ),
+                        ], ["^persona\.fields\.OrderedCountryField"])
