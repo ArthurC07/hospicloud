f4ac948f3f23f9959711a04e54d8e1d49d7542db
Carlos Flores <cafg10@gmail.com>
2014-02-25 11:23:28 -0600

    Agregar chat interno y ayuda en línea

--- hospinet/hospinet/context_processors.py
+++ hospinet/hospinet/context_processors.py
@@ -14,5 +14,9 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
+from constance import config
 
-def chat
+
+def chat(request):
+
+    return {'online_help': config.ONLINE_HELP, 'chat': config.CHAT}
