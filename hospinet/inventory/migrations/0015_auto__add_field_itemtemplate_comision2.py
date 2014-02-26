da3c75cb4f73fba05291587c461284f8c7ef3896
Carlos Flores <cafg10@gmail.com>
2014-02-26 10:04:53 -0600

    Incluir técnico en la creación del recibo de examen

---
+++ hospinet/inventory/migrations/0015_auto__add_field_itemtemplate_comision2.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'ItemTemplate.comision2'
+        db.add_column(u'inventory_itemtemplate', 'comision2',
+                      self.gf('django.db.models.fields.DecimalField')(default='10.00', max_digits=4, decimal_places=2),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'ItemTemplate.comision2'
+        db.delete_column(u'inventory_itemtemplate', 'comision2')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'inventory.compra': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'Compra'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ingresada': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'inventario': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'compras'", 'null': 'True', 'to': u"orm['inventory.Inventario']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'proveedor': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'inventory.historial': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'Historial'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'fecha': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2014, 2, 26, 0, 0)'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inventario': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'historiales'", 'to': u"orm['inventory.Inventario']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'})
+        },
+        u'inventory.inventario': {
+            'Meta': {'object_name': 'Inventario'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lugar': ('django.db.models.fields.CharField', [], {'default': "'Bodega'", 'max_length': '255'}),
+            'puede_comprar': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'blank': 'True'})
+        },
+        u'inventory.item': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'Item'},
+            'cantidad': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inventario': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'items'", 'to': u"orm['inventory.Inventario']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'plantilla': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'items'", 'to': u"orm['inventory.ItemTemplate']"})
+        },
+        u'inventory.itemaction': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'ItemAction'},
+            'action': ('django.db.models.fields.TextField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'acciones'", 'to': u"orm['inventory.ItemTemplate']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        },
+        u'inventory.itemcomprado': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'ItemComprado'},
+            'cantidad': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'compra': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'items'", 'to': u"orm['inventory.Compra']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ingresado': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'comprado'", 'null': 'True', 'to': u"orm['inventory.ItemTemplate']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'})
+        },
+        u'inventory.itemhistorial': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'ItemHistorial'},
+            'cantidad': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'historial': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'items'", 'to': u"orm['inventory.Historial']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'historicos'", 'to': u"orm['inventory.ItemTemplate']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'})
+        },
+        u'inventory.itemrequisicion': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'ItemRequisicion'},
+            'cantidad': ('django.db.models.fields.IntegerField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'entregada': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'requisiciones'", 'to': u"orm['inventory.ItemTemplate']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'pendiente': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'requisicion': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'items'", 'to': u"orm['inventory.Requisicion']"})
+        },
+        u'inventory.itemtemplate': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'ItemTemplate'},
+            'activo': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'comision': ('django.db.models.fields.DecimalField', [], {'default': "'30.00'", 'max_digits': '4', 'decimal_places': '2'}),
+            'comision2': ('django.db.models.fields.DecimalField', [], {'default': "'10.00'", 'max_digits': '4', 'decimal_places': '2'}),
+            'costo': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '10', 'decimal_places': '2'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'descripcion': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'impuestos': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '10', 'decimal_places': '2'}),
+            'item_type': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'items'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['inventory.ItemType']"}),
+            'marca': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'modelo': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'notas': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'precio_de_venta': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '10', 'decimal_places': '2'}),
+            'suppliers': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'plantillas'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['inventory.Proveedor']"}),
+            'unidad_de_medida': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'})
+        },
+        u'inventory.itemtype': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'ItemType'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'nombre': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'inventory.proveedor': {
+            'Meta': {'object_name': 'Proveedor'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'inventory.requisicion': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'Requisicion'},
+            'aprobada': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'entregada': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inventario': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'requisiciones'", 'null': 'True', 'to': u"orm['inventory.Inventario']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'usuario': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'requisiciones'", 'null': 'True', 'to': u"orm['auth.User']"})
+        },
+        u'inventory.tipoventa': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'TipoVenta'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'descripcion': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'disminucion': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '10', 'decimal_places': '2'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'incremento': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '10', 'decimal_places': '2'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'})
+        },
+        u'inventory.transferencia': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'Transferencia'},
+            'aplicada': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'destino': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'entradas'", 'null': 'True', 'to': u"orm['inventory.Inventario']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'origen': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'salidas'", 'null': 'True', 'to': u"orm['inventory.Inventario']"}),
+            'requisicion': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'transferencias'", 'null': 'True', 'to': u"orm['inventory.Requisicion']"}),
+            'usuario': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'transferencias'", 'null': 'True', 'to': u"orm['auth.User']"})
+        },
+        u'inventory.transferido': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'Transferido'},
+            'aplicada': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'cantidad': ('django.db.models.fields.IntegerField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'transferidos'", 'to': u"orm['inventory.ItemTemplate']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'transferencia': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'transferidos'", 'to': u"orm['inventory.Transferencia']"})
+        }
+    }
+
+    complete_apps = ['inventory']
