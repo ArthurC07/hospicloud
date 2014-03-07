967ba6c28c692cdb8124f0d19644663efd07152d
Carlos Flores <cafg10@gmail.com>
2014-03-06 20:51:03 -0600

    PEP 8

--- hospinet/persona/urls.py
+++ hospinet/persona/urls.py
@@ -16,62 +16,66 @@
 
 from django.conf.urls import patterns, url
 from persona.views import (PersonaDetailView, PersonaCreateView,
-    PersonaUpdateView, EstiloVidaUpdateView, AntecedenteUpdateView,
-    AntecedenteFamiliarUpdateView, AntecedenteObstetricoUpdateView,
-    PersonaIndexView, FisicoUpdateView, AntecedenteQuirurgicoUpdateView,
-    AntecedenteQuirurgicoCreateView, PersonaSearchView)
+                           PersonaUpdateView, EstiloVidaUpdateView,
+                           AntecedenteUpdateView,
+                           AntecedenteFamiliarUpdateView,
+                           AntecedenteObstetricoUpdateView,
+                           PersonaIndexView, FisicoUpdateView,
+                           AntecedenteQuirurgicoUpdateView,
+                           AntecedenteQuirurgicoCreateView, PersonaSearchView)
 
 urlpatterns = patterns('',
-    
-    url(r'^$',
-        PersonaIndexView.as_view(),
-        name='persona-index'),
-    
-    url(r'^(?P<pk>\d+)$',
-        PersonaDetailView.as_view(),
-        name='persona-view-id'),
-    
-    url(r'^(?P<pk>\d+)/estilovida$',
-        PersonaDetailView.as_view(template_name='persona/estilo_detail.html'),
-        name='persona-estilo'),
-    
-    url(r'^agregar$',
-        PersonaCreateView.as_view(),
-        name='persona-create'),
-    
-    url(r'^buscar$',
-        PersonaSearchView.as_view(),
-        name='persona-search'),
-    
-    url(r'^(?P<pk>\d+)/editar$',
-        PersonaUpdateView.as_view(),
-        name='persona-editar'),
-    
-    url(r'^(?P<pk>\d+)/fisico/editar$',
-        FisicoUpdateView.as_view(),
-        name='persona-fisico-editar'),
-    
-    url(r'^(?P<pk>\d+)/estilovida/editar$',
-        EstiloVidaUpdateView.as_view(),
-        name='persona-estilovida-editar'),
-    
-    url(r'^(?P<pk>\d+)/antecedente/editar$',
-        AntecedenteUpdateView.as_view(),
-        name='persona-antecedente-editar'),
-    
-    url(r'^(?P<pk>\d+)/antecedente/familiar/editar$',
-        AntecedenteFamiliarUpdateView.as_view(),
-        name='persona-antecedente-familiar-editar'),
-    
-    url(r'^(?P<pk>\d+)/antecedente/quirurgico/editar$',
-        AntecedenteQuirurgicoUpdateView.as_view(),
-        name='persona-antecedente-quirurgico-editar'),
-    
-    url(r'^(?P<pk>\d+)/antecedente/quirurgico/agregar$',
-        AntecedenteQuirurgicoCreateView.as_view(),
-        name='persona-antecedente-quirurgico-agregar'),
 
-    url(r'^(?P<pk>\d+)/antecedente/obstetrico/editar$',
-        AntecedenteObstetricoUpdateView.as_view(),
-        name='persona-antecedente-obstetrico-editar'),
+                       url(r'^$',
+                           PersonaIndexView.as_view(),
+                           name='persona-index'),
+
+                       url(r'^(?P<pk>\d+)$',
+                           PersonaDetailView.as_view(),
+                           name='persona-view-id'),
+
+                       url(r'^(?P<pk>\d+)/estilovida$',
+                           PersonaDetailView.as_view(
+                               template_name='persona/estilo_detail.html'),
+                           name='persona-estilo'),
+
+                       url(r'^agregar$',
+                           PersonaCreateView.as_view(),
+                           name='persona-create'),
+
+                       url(r'^buscar$',
+                           PersonaSearchView.as_view(),
+                           name='persona-search'),
+
+                       url(r'^(?P<pk>\d+)/editar$',
+                           PersonaUpdateView.as_view(),
+                           name='persona-editar'),
+
+                       url(r'^(?P<pk>\d+)/fisico/editar$',
+                           FisicoUpdateView.as_view(),
+                           name='persona-fisico-editar'),
+
+                       url(r'^(?P<pk>\d+)/estilovida/editar$',
+                           EstiloVidaUpdateView.as_view(),
+                           name='persona-estilovida-editar'),
+
+                       url(r'^(?P<pk>\d+)/antecedente/editar$',
+                           AntecedenteUpdateView.as_view(),
+                           name='persona-antecedente-editar'),
+
+                       url(r'^(?P<pk>\d+)/antecedente/familiar/editar$',
+                           AntecedenteFamiliarUpdateView.as_view(),
+                           name='persona-antecedente-familiar-editar'),
+
+                       url(r'^(?P<pk>\d+)/antecedente/quirurgico/editar$',
+                           AntecedenteQuirurgicoUpdateView.as_view(),
+                           name='persona-antecedente-quirurgico-editar'),
+
+                       url(r'^(?P<pk>\d+)/antecedente/quirurgico/agregar$',
+                           AntecedenteQuirurgicoCreateView.as_view(),
+                           name='persona-antecedente-quirurgico-agregar'),
+
+                       url(r'^(?P<pk>\d+)/antecedente/obstetrico/editar$',
+                           AntecedenteObstetricoUpdateView.as_view(),
+                           name='persona-antecedente-obstetrico-editar'),
 )
