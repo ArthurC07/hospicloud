69900e99fac439043b54ba840bd4c39218cc849b
Carlos Flores <cafg10@gmail.com>
2014-04-01 11:31:48 -0600

    Agregar tipo de pago al admin

--- hospinet/contracts/admin.py
+++ hospinet/contracts/admin.py
@@ -17,7 +17,7 @@
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
 from django.contrib import admin
 from contracts.models import (TipoEvento, Plan, Contrato, Evento, Pago,
-                              Beneficiario, Vendedor, LimiteEvento)
+                              Beneficiario, Vendedor, LimiteEvento, TipoPago)
 
 admin.site.register(Plan)
 admin.site.register(Contrato)
@@ -27,3 +27,4 @@ admin.site.register(LimiteEvento)
 admin.site.register(Pago)
 admin.site.register(Vendedor)
 admin.site.register(Beneficiario)
+admin.site.register(TipoPago)
