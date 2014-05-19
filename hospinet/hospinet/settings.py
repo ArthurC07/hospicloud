3c05aa278fc9a89ae65d5247aa52a2648c8f2bce
Carlos Flores <cafg10@gmail.com>
2014-05-19 11:55:51 -0600

    Weird

--- hospinet/hospinet/settings.py
+++ hospinet/hospinet/settings.py
@@ -288,3 +288,7 @@ CONSTANCE_CONFIG = {
 #EMAIL_HOST_PASSWORD = 'password'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+SOUTH_MIGRATION_MODULES = {
+    'easy_thumbnails': 'easy_thumbnails.south_migrations',
+}
