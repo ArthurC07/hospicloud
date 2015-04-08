d95ba4b91624d9584be2eb11445b858c05636949
Carlos Flores <cafg10@gmail.com>
2015-04-08 16:26:54 -0600

    Added required migrations

---
+++ hospinet/clinique/migrations/0009_auto_20150408_1626.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('clinique', '0008_auto_20150408_1621'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Especialidad',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('nombre', models.CharField(max_length=50)),
+                ('habilitado', models.BooleanField(default=True)),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='consultorio',
+            name='especialidad',
+            field=models.ForeignKey(related_name='consultorios', blank=True, to='clinique.Especialidad', null=True),
+            preserve_default=True,
+        ),
+    ]
