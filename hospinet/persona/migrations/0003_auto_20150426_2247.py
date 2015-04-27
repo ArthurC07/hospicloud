5e4cfb607cf1549647661d248130c9ef5bdd0e5e
Carlos Flores <cafg10@gmail.com>
2015-04-26 22:55:10 -0600

    Fixing data before migrating

--- hospinet/persona/migrations/0003_auto_20150426_2247.py
+++ hospinet/persona/migrations/0003_auto_20150426_2247.py
@@ -4,6 +4,27 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
+def fix_antecedente(apps, schema_editor):
+    Antecedente = apps.get_model("persona", "Antecedente")
+
+    for antecedente in Antecedente.objects.all():
+
+        if antecedente.alcoholismo is None:
+            antecedente.alcoholismo = False
+        if antecedente.cancer is None:
+            antecedente.cancer = False
+        if antecedente.colesterol is None:
+            antecedente.colesterol = False
+        if antecedente.obesidad is None:
+            antecedente.obesidad = False
+        if antecedente.trigliceridos is None:
+            antecedente.trigliceridos = False
+        if antecedente.migrana is None:
+            antecedente.migrana = False
+            
+        antecedente.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,6 +32,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(fix_antecedente),
         migrations.AlterField(
             model_name='antecedente',
             name='alcoholismo',
