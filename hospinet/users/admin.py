0df28f1bf9b6b56530469fc16ac029848e8fe983
Carlos Flores <cafg10@gmail.com>
2015-05-22 21:46:13 -0600

    Update Users admin

--- hospinet/users/admin.py
+++ hospinet/users/admin.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from users.models import UserProfile
+
+from users.models import Ciudad
 
 # admin.site.register(UserProfile)
+
+
+class CiudadAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'correlativo', 'prefijo_recibo')
+    ordering = ['nombre', 'correlativo', 'prefijo_recibo']
+    search_fields = ['nombre', 'correlativo', 'prefijo_recibo']
+
+
+admin.site.register(Ciudad, CiudadAdmin)
