4ec8fb99f57b162d4422af0c0acf54e1578dac6a
Carlos Flores <cafg10@gmail.com>
2015-05-22 21:47:05 -0600

    Incomplete variable name

--- hospinet/users/admin.py
+++ hospinet/users/admin.py
@@ -7,9 +7,9 @@ from users.models import Ciudad
 
 
 class CiudadAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'correlativo', 'prefijo_recibo')
-    ordering = ['nombre', 'correlativo', 'prefijo_recibo']
-    search_fields = ['nombre', 'correlativo', 'prefijo_recibo']
+    list_display = ('nombre', 'correlativo_de_recibo', 'prefijo_recibo')
+    ordering = ['nombre', 'correlativo_de_recibo', 'prefijo_recibo']
+    search_fields = ['nombre', 'correlativo_de_recibo', 'prefijo_recibo']
 
 
 admin.site.register(Ciudad, CiudadAdmin)
