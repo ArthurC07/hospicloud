479b3630904268306f6bf18da02aa953bc93188f
Carlos Flores <cafg10@gmail.com>
2015-07-24 11:32:51 -0600

    Modernize settings.py

---
+++ hospinet/hospinet/project_settings.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Carlos Flores <cafg10@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+# Django settings for hospinet project.
+
+CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
+CONSTANCE_CONFIG = {
+    'RTN': (u'', u'Registro Tributario Nacional'),
+    'RECEIPT_ROOT': (u'PlaceHolder', u'Prefijo para recibos'),
+    'COMPANY_NAME': (u'COMPANY_NAME', u'Nombre de la Empresa'),
+    'COMPANY_ADDRESS': (u'', u'Dirección de la Compañía'),
+    'EMERGENCIA': (1, u'Cuenta utilizada para estadia en emergencias'),
+    'INVOICE_OFFSET': (0, u'Numeración para iniciar recibos'),
+    'DEPOSIT_ACCOUNT': (1, u'Cuenta utilizada para disminuir depositos'),
+    'EXTRA_EMERGENCIA': (1, u'Cuenta utilizada para agregar tiempo extra de emergencias'),
+    'DEPOSIT_PAYMENT': (1, u'Tipo de Pago para Abonos a cuenta'),
+    'CHAT': (u'http://www.example.com', u'Url para el chat interno'),
+    'ONLINE_HELP': (u'http://www.example.com', u'Url para ayuda en línea'),
+    'SYSTEM_EMAIL': (u'me@localhost', u'Email utilizado para enviar correo'),
+    'NOTIFICATION_EMAIL': (u'me@localhost', u'Email para notificar'),
+    'DEFAULT_VENTA_TYPE': (1, u'Tipo de Venta Predeterminada'),
+    'DEFAULT_CONSULTA_ITEM': (1, u'Costo de Consulta'),
+    'NIGHT_CONSULTA_ITEM': (1, u'Costo de Consulta Nocturna'),
+    'ELDER_VENTA_TYPE': (1, u'Tipo de Venta Predeterminada'),
+    'ELDER_AGE': (60, u'Edad mínima de la Tercera Edad'),
+    'CONSULTA_ENABLED': (1, u'Consulta habilitada'),
+    'BUDGET_ENABLED': (1, u'Presupuesto Habilitado'),
+    'EMERGENCIA_ENABLED': (1, u'Emergencia Habilitada'),
+    'HOSPITALIZACION_ENABLED': (1, u'Hospitalizacion Habilitada'),
+    'IMAGENES_ENABLED': (1, u'Imagenes Habilitadas'),
+    'CONTRATOS_ENABLED': (1, u'Contratos Habilitados'),
+    'INVENTARIO_ENABLED': (1, u'Inventario Habilitado'),
+    'RRHH_ENABLED': (1, u'Recursos Humanos Habilitado'),
+    'LAB_ENABLED': (1, u'Laboratorio Habilitado'),
+    'INVOICE_ENABLED': (1, u'Caja habilitada'),
+    'DEFAULT_CLIENT': (1, u'Cliente Predeterminado para los recibos'),
+    'RECEIPT_DAYS': (1, u'Dias que dura una factura al credito'),
+    'PHONE_NUMBER': (1, u'Número de teléfono'),
+    'CAI': ('1', u'CAI'),
+    'CURRENCY_EXCHANGE': (22.03, u'Cambio de Moneda'),
+    'PAYMENT_TYPE_PENDING': (1, u'Tipo de Pago Pendiente'),
+    'PAYMENT_STATUS_PENDING': (1, u'Estado de Pago Pendiente'),
+}
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+USERENA_ACTIVATION_REQUIRED = False
+AUTH_PROFILE_MODULE = 'users.UserProfile'
--- hospinet/hospinet/settings.py
+++ hospinet/hospinet/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2011-2013 Carlos Flores <cafg10@gmail.com>
+# Copyright (C) 2011-2015 Carlos Flores <cafg10@gmail.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -18,10 +18,9 @@
 # Django settings for hospinet project.
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-
+from project_settings import *
 
 def env_var(key, default=None):
     """Retrieves env vars and makes Python boolean replacements"""
@@ -32,17 +31,9 @@ def env_var(key, default=None):
         val = False
     return val
 
-
-COMPANY = None
-if COMPANY != None:
-    LOGIN_URL = '/{0}/accounts/signin/'.format(COMPANY)
-    LOGOUT_URL = '/{0}/accounts/signout/'.format(COMPANY)
-    LOGIN_REDIRECT_URL = '/{0}/accounts/%(username)s/'.format(COMPANY)
-#COMPANY = 'hospinet'
-else:
-    LOGIN_URL = '/accounts/signin/'
-    LOGOUT_URL = '/accounts/signout/'
-    LOGIN_REDIRECT_URL = '/accounts/%(username)s/'
+LOGIN_URL = '/accounts/signin/'
+LOGOUT_URL = '/accounts/signout/'
+LOGIN_REDIRECT_URL = '/accounts/%(username)s/'
 
 DEBUG = env_var('DEBUG', True)
 TEMPLATE_DEBUG = env_var('DEBUG', True)
@@ -96,42 +87,27 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
+MEDIA_ROOT = 'media'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+STATIC_ROOT = 'static'
 
-if COMPANY != None:
-    # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-    # trailing slash.
-    # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-    MEDIA_URL = '/{0}/media/'.format(COMPANY)
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
 
-    # URL prefix for static files.
-    # Example: "http://media.lawrence.com/static/"
-    STATIC_URL = '/{0}/static/'.format(COMPANY)
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
 
-    # URL prefix for admin static files -- CSS, JavaScript and images.
-    # Make sure to use a trailing slash.
-    # Examples: "http://foo.com/static/admin/", "/static/admin/".
-    ADMIN_MEDIA_PREFIX = '/{0}/static/admin/'.format(COMPANY)
-else:
-    # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-    # trailing slash.
-    # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-    MEDIA_URL = '/media/'
-
-    # URL prefix for static files.
-    # Example: "http://media.lawrence.com/static/"
-    STATIC_URL = '/static/'
-
-    # URL prefix for admin static files -- CSS, JavaScript and images.
-    # Make sure to use a trailing slash.
-    # Examples: "http://foo.com/static/admin/", "/static/admin/".
-    ADMIN_MEDIA_PREFIX = '/static/admin/'
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -145,35 +121,42 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '%d3r*q0fk6#5y-j%88zn#f+pq16)v2x6ap%q_y)7dj+r59@_#^'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    #     'django.template.loaders.eggs.Loader',
-)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'hospinet.context_processors.chat'
+            ],
+        },
+    },
+]
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'hospinet.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, '../templates'),
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -216,42 +199,6 @@ INSTALLED_APPS = (
     'constance.backends.database',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request',
-    'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
-    'hospinet.context_processors.chat'
-)
-
 AUTHENTICATION_BACKENDS = (
     'userena.backends.UserenaAuthenticationBackend',
     'guardian.backends.ObjectPermissionBackend',
@@ -268,54 +215,12 @@ CACHES = {
 ANONYMOUS_USER_ID = -1
 AUTH_PROFILE_MODULE = 'users.UserProfile'
 USE_THOUSAND_SEPARATOR = True
-USERENA_ACTIVATION_REQUIRED = False
-
-CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
-CONSTANCE_CONFIG = {
-    'RTN': (u'', u'Registro Tributario Nacional'),
-    'RECEIPT_ROOT': (u'PlaceHolder', u'Prefijo para recibos'),
-    'COMPANY_NAME': (u'COMPANY_NAME', u'Nombre de la Empresa'),
-    'COMPANY_ADDRESS': (u'', u'Dirección de la Compañía'),
-    'EMERGENCIA': (1, u'Cuenta utilizada para estadia en emergencias'),
-    'INVOICE_OFFSET': (0, u'Numeración para iniciar recibos'),
-    'DEPOSIT_ACCOUNT': (1, u'Cuenta utilizada para disminuir depositos'),
-    'EXTRA_EMERGENCIA': (1, u'Cuenta utilizada para agregar tiempo extra de emergencias'),
-    'DEPOSIT_PAYMENT': (1, u'Tipo de Pago para Abonos a cuenta'),
-    'CHAT': (u'http://www.example.com', u'Url para el chat interno'),
-    'ONLINE_HELP': (u'http://www.example.com', u'Url para ayuda en línea'),
-    'SYSTEM_EMAIL': (u'me@localhost', u'Email utilizado para enviar correo'),
-    'NOTIFICATION_EMAIL': (u'me@localhost', u'Email para notificar'),
-    'DEFAULT_VENTA_TYPE': (1, u'Tipo de Venta Predeterminada'),
-    'DEFAULT_CONSULTA_ITEM': (1, u'Costo de Consulta'),
-    'NIGHT_CONSULTA_ITEM': (1, u'Costo de Consulta Nocturna'),
-    'ELDER_VENTA_TYPE': (1, u'Tipo de Venta Predeterminada'),
-    'ELDER_AGE': (60, u'Edad mínima de la Tercera Edad'),
-    'CONSULTA_ENABLED': (1, u'Consulta habilitada'),
-    'BUDGET_ENABLED': (1, u'Presupuesto Habilitado'),
-    'EMERGENCIA_ENABLED': (1, u'Emergencia Habilitada'),
-    'HOSPITALIZACION_ENABLED': (1, u'Hospitalizacion Habilitada'),
-    'IMAGENES_ENABLED': (1, u'Imagenes Habilitadas'),
-    'CONTRATOS_ENABLED': (1, u'Contratos Habilitados'),
-    'INVENTARIO_ENABLED': (1, u'Inventario Habilitado'),
-    'RRHH_ENABLED': (1, u'Recursos Humanos Habilitado'),
-    'LAB_ENABLED': (1, u'Laboratorio Habilitado'),
-    'INVOICE_ENABLED': (1, u'Caja habilitada'),
-    'DEFAULT_CLIENT': (1, u'Cliente Predeterminado para los recibos'),
-    'RECEIPT_DAYS': (1, u'Dias que dura una factura al credito'),
-    'PHONE_NUMBER': (1, u'Número de teléfono'),
-    'CAI': ('1', u'CAI'),
-    'CURRENCY_EXCHANGE': (22.03, u'Cambio de Moneda'),
-    'PAYMENT_TYPE_PENDING': (1, u'Tipo de Pago Pendiente'),
-    'PAYMENT_STATUS_PENDING': (1, u'Estado de Pago Pendiente'),
-}
 
 
 #EMAIL_HOST = 'smtp.gmail.com'
 #EMAIL_PORT = 587
 #EMAIL_HOST_USER = 'me@gmail.com'
 #EMAIL_HOST_PASSWORD = 'password'
-
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 WSGI_APPLICATION = 'hospinet.wsgi.application'
