f2abd8d50ef2e44dad38956d4248c6bc9dd4ca2f
Carlos Flores <cafg10@gmail.com>
2015-08-31 12:01:14 -0600

    Adding to pythonpath

--- hospinet/wsgi.py
+++ hospinet/wsgi.py
@@ -24,8 +24,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hospinet.settings")
 from dj_static import Cling
