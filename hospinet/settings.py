989315d76dc220bc6298410b2e1fbaddc7ac40b1
Carlos Flores <cafg10@gmail.com>
2015-08-31 23:45:22 -0600

    Remove hospinet references

--- hospinet/settings.py
+++ hospinet/settings.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-# Django settings for hospinet project.
+# Django settings for hospicloud project.
 import os
 
 import environ
