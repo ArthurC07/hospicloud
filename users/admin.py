f75c1b93d53552a21a46e79dc72a46da7673c02c
Carlos Flores <cafg10@gmail.com>
2015-09-02 18:40:32 -0600

    Add Company to admin

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from users.models import Ciudad
+from users.models import Ciudad, Company
 
 # admin.site.register(UserProfile)
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'rtn', 'cai')
+    ordering = ['nombre', 'rtn', 'cai']
+    search_fields = ['nombre', 'rtn', 'cai']
 
 
 class CiudadAdmin(admin.ModelAdmin):
@@ -12,4 +17,5 @@ class CiudadAdmin(admin.ModelAdmin):
     search_fields = ['nombre', 'correlativo_de_recibo', 'prefijo_recibo']
 
 
+admin.site.register(Company, CompanyAdmin)
 admin.site.register(Ciudad, CiudadAdmin)
