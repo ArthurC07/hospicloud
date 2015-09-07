cf2516c30344b87d28e5de18358490df4820f262
Carlos Flores <cafg10@gmail.com>
2015-09-07 16:54:41 -0600

    Maybe the third works

--- nightingale/migrations/0001_initial.py
+++ nightingale/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('inventory', '0001_squashed_0019_auto_20150729_1025'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('spital', '0001_squashed_0019_auto_20150729_1025'),
+        ('spital', '0001_squashed_0004_remove_admision_uuid'),
     ]
 
     operations = [
