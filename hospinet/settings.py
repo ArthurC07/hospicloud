73f94c76657a33911147ec861ffc3f5fe7162029
Carlos Flores <cafg10@gmail.com>
2015-09-07 09:03:36 -0600

    Leave this settings this way

--- hospinet/settings.py
+++ hospinet/settings.py
@@ -64,13 +64,13 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = root.path('media')
+MEDIA_ROOT = 'media'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = root.path('static')
+STATIC_ROOT = 'static'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
