010bfa9397cc2a4c48950a3f4cccd50714a8f29d
Carlos Flores <cafg10@gmail.com>
2015-09-07 16:05:54 -0600

    Squashing some more migrations

---
+++ spital/migrations/0001_squashed_0004_remove_admision_uuid.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    replaces = [(b'spital', '0001_initial'), (b'spital', '0002_delete_especialidad'), (b'spital', '0003_auto_20150507_1637'), (b'spital', '0004_remove_admision_uuid')]
+
+    dependencies = [
+        ('inventory', '0001_squashed_0019_auto_20150729_1025'),
+        ('emergency', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('persona', '0001_squashed_0013_persona_mostrar_en_cardex'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Admision',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('momento', models.DateTimeField(default=django.utils.timezone.now, null=True, blank=True)),
+                ('diagnostico', models.CharField(max_length=200, blank=True)),
+                ('doctor', models.CharField(max_length=200, blank=True)),
+                ('arancel', models.CharField(blank=True, max_length=200, choices=[(b'E', 'Empleado'), (b'J', 'Ejecutivo'), (b'X', 'Extranjero')])),
+                ('pago', models.CharField(blank=True, max_length=200, choices=[(b'EF', 'Efectivo'), (b'CK', 'Cheque'), (b'CO', 'Empresa'), (b'OC', 'Orden de Compra'), (b'TC', 'Tarjeta Cr\xe9dito'), (b'TB', 'Transferencia Bancaria')])),
+                ('poliza', models.CharField(max_length=200, blank=True)),
+                ('certificado', models.CharField(max_length=200, blank=True)),
+                ('aseguradora', models.CharField(max_length=200, blank=True)),
+                ('deposito', models.CharField(max_length=200, blank=True)),
+                ('observaciones', models.CharField(max_length=200, blank=True)),
+                ('admision', models.DateTimeField(default=django.utils.timezone.now, null=True, blank=True)),
+                ('autorizacion', models.DateTimeField(default=django.utils.timezone.now, null=True, blank=True)),
+                ('hospitalizacion', models.DateTimeField(null=True, blank=True)),
+                ('ingreso', models.DateTimeField(null=True, blank=True)),
+                ('fecha_pago', models.DateTimeField(default=django.utils.timezone.now, null=True, blank=True)),
+                ('fecha_alta', models.DateTimeField(default=django.utils.timezone.now, null=True, blank=True)),
+                ('estado', models.CharField(blank=True, max_length=1, choices=[(b'A', b'Admitido'), (b'B', b'Autorizado'), (b'H', b'Hospitalizar'), (b'I', b'Ingresado'), (b'C', b'Alta'), (b'Q', b'Cancelada')])),
+                ('tiempo', models.IntegerField(default=0, blank=True)),
+                ('neonato', models.NullBooleanField()),
+                ('tipo_de_ingreso', models.CharField(blank=True, max_length=200, null=True, choices=[(b'PA', b'Particular'), (b'SN', b'Aseguradora Nacional'), (b'SI', b'Aseguradora Internacional'), (b'PS', b'Presupuesto')])),
+                ('facturada', models.NullBooleanField(default=False)),
+                ('ultimo_cobro', models.DateTimeField(default=django.utils.timezone.now, null=True, blank=True)),
+                ('admitio', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('fiadores', models.ManyToManyField(related_name='fianzas', null=True, to=b'persona.Persona', blank=True)),
+            ],
+            options={
+                'permissions': (('admision', 'Permite al usuario gestionar admision'),),
+            },
+        ),
+        migrations.CreateModel(
+            name='Deposito',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('monto', models.DecimalField(null=True, max_digits=7, decimal_places=2, blank=True)),
+                ('fecha', models.DateTimeField(default=django.utils.timezone.now, null=True, blank=True)),
+                ('recibo', models.IntegerField(null=True, blank=True)),
+                ('admision', models.ForeignKey(related_name='depositos', to='spital.Admision')),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+        ),
+        migrations.CreateModel(
+            name='Doctor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('nombre', models.CharField(max_length=50)),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+        ),
+        migrations.CreateModel(
+            name='Habitacion',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('numero', models.IntegerField()),
+                ('tipo', models.CharField(blank=True, max_length=1, choices=[(b'N', b'Normal'), (b'S', b'Suite'), (b'U', b'U.C.I.')])),
+                ('estado', models.CharField(blank=True, max_length=1, choices=[(b'D', b'Disponible'), (b'O', b'Ocupada'), (b'M', b'Mantenimiento')])),
+                ('item', models.ForeignKey(related_name='habitaciones', blank=True, to='inventory.ItemTemplate', null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Laboratorio',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('nombre', models.CharField(max_length=50)),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+        ),
+        migrations.CreateModel(
+            name='PreAdmision',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('completada', models.BooleanField(default=False)),
+                ('transferir_cobros', models.BooleanField(default=False)),
+                ('emergencia', models.ForeignKey(related_name='preadmisiones', to='emergency.Emergencia')),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+        ),
+        migrations.AddField(
+            model_name='admision',
+            name='habitacion',
+            field=models.ForeignKey(related_name='admisiones', blank=True, to='spital.Habitacion', null=True),
+        ),
+        migrations.AddField(
+            model_name='admision',
+            name='paciente',
+            field=models.ForeignKey(related_name='admisiones', to='persona.Persona'),
+        ),
+        migrations.AddField(
+            model_name='admision',
+            name='referencias',
+            field=models.ManyToManyField(related_name='referencias', to=b'persona.Persona', blank=True),
+        ),
+        migrations.AddField(
+            model_name='admision',
+            name='tipo_de_venta',
+            field=models.ForeignKey(blank=True, to='inventory.TipoVenta', null=True),
+        ),
+        migrations.AlterField(
+            model_name='admision',
+            name='fiadores',
+            field=models.ManyToManyField(related_name='fianzas', to=b'persona.Persona', blank=True),
+        ),
+    ]
