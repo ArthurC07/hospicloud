b91ba0ec496bff08c87b11b69b0c2f1a8961858a
Carlos Flores <cafg10@gmail.com>
2015-12-30 10:55:15 -0600

    Fixed a response problem

--- income/views.py
+++ income/views.py
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
+from django.http import HttpResponseRedirect
 from django.views.generic import CreateView, ListView, DetailView
 
 from income.forms import ChequeCobroForm, DetallePagoForm
@@ -79,7 +80,7 @@ class ChequeCobroCreateView(CreateView, LoginRequiredMixin):
                 detalle.pago = pago
                 detalle.save()
 
-        return self.get_success_url()
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class ChequeCobroDetailView(DetailView, LoginRequiredMixin):
