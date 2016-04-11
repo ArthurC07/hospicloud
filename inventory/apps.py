225eff480f8ac854b6c88c362c5c3acb3e39df4a
Carlos Flores <cafg10@gmail.com>
2016-04-11 08:37:08 -0600

    Updating inventory to Django 1.9 standards

--- inventory/apps.py
+++ inventory/apps.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-#
-# Copyright (C) 2016 Carlos Flores <cafg10@gmail.com>
+# Copyright (C) 2015 Carlos Flores <cafg10@gmail.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -14,4 +13,12 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
-default_app_config = 'inventory.apps.InventoryConfig'
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class InventoryConfig(AppConfig):
+    name = 'inventory'
+    verbose_name = _('Inventario')
