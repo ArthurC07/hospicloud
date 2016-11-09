f20209ab54f83eee2ef14d152703ef5a6c204718
Carlos Flores <cafg10@gmail.com>
2016-11-09 12:46:17 -0600

    Optimizing imports

--- hospinet/utils/views.py
+++ hospinet/utils/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import simplejson
 from django.contrib import messages
 from django.core import serializers
 from django.core.urlresolvers import reverse
